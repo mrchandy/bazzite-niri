df359fc8f27ba2324027eac2e54bb66ba59d8f37
mrchandy <chndler.arhc@gmail.com>
2026-02-01 14:25:29 -0700

    feat(Nautilus): Add xdg-terminal-exec and the needed nautlius extension

--- build_files/install-nautilus.sh
+++ build_files/install-nautilus.sh
@@ -3,6 +3,7 @@
 # Install Nautilus, and thumbnailers
 dnf -y install \
     nautilus \
+    nautilus-python \
     glycin-thumbnailer \
     mcomix3-thumbnailer \
     evince-thumbnailer \
@@ -14,3 +15,9 @@ dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-m
 dnf config-manager setopt fedora-multimedia.enabled=0
 dnf -y install --enablerepo=fedora-multimedia \
     libavcodec @multimedia gstreamer1-plugins-{bad-free,bad-free-libs,good,base} lame{,-libs} libjxl ffmpegthumbnailer
+
+# Tulips workflow for the Nautilus xdg-terminal-exec extension
+XDG_EXT_TMPDIR="$(mktemp -d)"
+curl -fsSLo - "$(curl -fsSL https://api.github.com/repos/tulilirockz/xdg-terminal-exec-nautilus/releases/latest | jq -rc .tarball_url)" | tar -xzvf - -C "${XDG_EXT_TMPDIR}"
+install -Dpm0644 -t "/usr/share/nautilus-python/extensions/" "${XDG_EXT_TMPDIR}"/*/xdg-terminal-exec-nautilus.py
+rm -rf "${XDG_EXT_TMPDIR}"
