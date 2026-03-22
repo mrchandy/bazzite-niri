5bb4bf50188db19f5f40cb32cc3b622ba54311ea
mrchandy <chndler.arhc@gmail.com>
2026-03-22 16:03:49 -0600

    Reapply "Merge remote-tracking branch 'upstream/unstable'."
    This reverts commit c9941e09d260ba56aa1b36f6049ad68a9aa30575.

--- .github/workflows/changelog.py
+++ .github/workflows/changelog.py
@@ -49,11 +49,16 @@ From previous `{target}` version `{prev}` there have been the following changes.
 | **Kernel** | {pkgrel:kernel} |
 | **Firmware** | {pkgrel:atheros-firmware} |
 | **Mesa** | {pkgrel:mesa-filesystem} |
-| **Gamescope** | {pkgrel:gamescope} |
+| **Gamescope** | {pkgrel:terra-gamescope} |
+| **Gamescope Session** | {pkgrel:gamescope-session} |
+| **InputPlumber** | {pkgrel:inputplumber} |
+| **OpenGamepadUI** | {pkgrel:opengamepadui} |
+| **PowerStation** | {pkgrel:powerstation} |
+| **SteamOS-Manager** | {pkgrel:steamos-manager-powerstation} |
+| **Bazaar** | {pkgrel:bazaar} |
 | **Niri** | {pkgrel:niri} |
 | **DMS** | {pkgrel:dms} |
 | **Quickshell** | {pkgrel:quickshell-git} |
-| **Bazaar** | {pkgrel:bazaar} |
 
 {changes}
 
@@ -72,7 +77,12 @@ This is an automatically generated changelog for release `{curr}`."""
 BLACKLIST_VERSIONS = [
     "kernel",
     "mesa-filesystem",
-    "gamescope",
+    "terra-gamescope",
+    "gamescope-session",
+    "inputplumber",
+    "powerstation",
+    "steamos-manager-powerstation",
+    "opengamepadui",
     "bazaar",
     "niri",
     "gnome-control-center-filesystem",
@@ -160,7 +170,7 @@ def get_packages(manifests: dict[str, Any]):
     packages = {}
     for img, manifest in manifests.items():
         try:
-            packages[img] = json.loads(manifest["Labels"]["dev.hhd.rechunk.info"])[
+            packages[img] = json.loads(manifest["Labels"]["ostree.rechunk.info"])[
                 "packages"
             ]
         except Exception as e:
